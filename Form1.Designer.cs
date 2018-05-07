809fc7d150c45aa0d61036cbdf79b4a58803ecc5
IMPRIVATAINC\radler <radler@imprivata.com>
2018-05-07 12:35:43 -0400

    Made it pop

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,27 +30,34 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
+            this.SelectTestSuite = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.ProjectSuiteSelector = new System.Windows.Forms.TextBox();
             this.ProjectSelector = new System.Windows.Forms.ComboBox();
             this.button2 = new System.Windows.Forms.Button();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.APFeedbackBox = new System.Windows.Forms.TextBox();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.Branch = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Dev = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.VersionBox = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.APGifZone = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.APGifZone)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // SelectTestSuite
             // 
-            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(12, 59);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(106, 20);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Select TestSuite ";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.SelectTestSuite.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SelectTestSuite.Location = new System.Drawing.Point(12, 59);
+            this.SelectTestSuite.Name = "SelectTestSuite";
+            this.SelectTestSuite.Size = new System.Drawing.Size(106, 20);
+            this.SelectTestSuite.TabIndex = 0;
+            this.SelectTestSuite.Text = "Select TestSuite ";
+            this.SelectTestSuite.UseVisualStyleBackColor = true;
+            this.SelectTestSuite.Click += new System.EventHandler(this.button1_Click);
             // 
             // openFileDialog1
             // 
@@ -78,7 +85,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(12, 112);
+            this.button2.Location = new System.Drawing.Point(12, 184);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(106, 23);
             this.button2.TabIndex = 4;
@@ -102,18 +109,79 @@
             this.APFeedbackBox.TabIndex = 6;
             this.APFeedbackBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged_1);
             // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(124, 184);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(108, 22);
+            this.resetButton.TabIndex = 7;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseMnemonic = false;
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // Branch
+            // 
+            this.Branch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Branch.Location = new System.Drawing.Point(55, 112);
+            this.Branch.Name = "Branch";
+            this.Branch.Size = new System.Drawing.Size(220, 20);
+            this.Branch.TabIndex = 8;
+            this.Branch.TextChanged += new System.EventHandler(this.Branch_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 112);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(60, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "Branch:";
+            // 
+            // Dev
+            // 
+            this.Dev.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Dev.Location = new System.Drawing.Point(55, 133);
+            this.Dev.Name = "Dev";
+            this.Dev.Size = new System.Drawing.Size(220, 20);
+            this.Dev.TabIndex = 10;
+            this.Dev.TextChanged += new System.EventHandler(this.Dev_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 133);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(60, 20);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.Text = "Dev:";
+            // 
+            // VersionBox
+            // 
+            this.VersionBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VersionBox.Location = new System.Drawing.Point(55, 154);
+            this.VersionBox.Name = "VersionBox";
+            this.VersionBox.Size = new System.Drawing.Size(220, 20);
+            this.VersionBox.TabIndex = 12;
+            this.VersionBox.TextChanged += new System.EventHandler(this.VersionBox_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 154);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(60, 20);
+            this.textBox4.TabIndex = 13;
+            this.textBox4.Text = "Version:";
+            // 
             // APGifZone
             // 
-            this.APGifZone.BackColor = System.Drawing.Color.Transparent;
-            this.APGifZone.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.APGifZone.Image = ((System.Drawing.Image)(resources.GetObject("APGifZone.Image")));
-            this.APGifZone.Location = new System.Drawing.Point(12, 141);
+            this.APGifZone.Location = new System.Drawing.Point(12, 248);
             this.APGifZone.Name = "APGifZone";
-            this.APGifZone.Size = new System.Drawing.Size(118, 218);
-            this.APGifZone.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.APGifZone.TabIndex = 5;
+            this.APGifZone.Size = new System.Drawing.Size(135, 111);
+            this.APGifZone.TabIndex = 14;
             this.APGifZone.TabStop = false;
-            this.APGifZone.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // Form1
             // 
@@ -123,12 +191,19 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(461, 367);
-            this.Controls.Add(this.APFeedbackBox);
             this.Controls.Add(this.APGifZone);
+            this.Controls.Add(this.VersionBox);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.Dev);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Branch);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.APFeedbackBox);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.ProjectSelector);
             this.Controls.Add(this.ProjectSuiteSelector);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SelectTestSuite);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Regression Suite Test Harness";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -140,13 +215,20 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button SelectTestSuite;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.TextBox ProjectSuiteSelector;
         private System.Windows.Forms.ComboBox ProjectSelector;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ImageList imageList1;
         private System.Windows.Forms.TextBox APFeedbackBox;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.TextBox Branch;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Dev;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox VersionBox;
+        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.PictureBox APGifZone;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 using System.Management.Automation;
 using System.Collections.ObjectModel;
 using System.Net;
-using System.IO; 
+using System.IO;
 
 namespace RegressionSuiteTestHarness
 {
@@ -19,12 +19,11 @@ namespace RegressionSuiteTestHarness
         public Form1()
         {
             InitializeComponent();
-            string[] project = { "PSAudit", "PSClient","PSClientInstallers","PSDatabase","PSHl7","PSI","PSIdentityService","PSMT","PSMT-Reports","PSServer","PSWS" };
+            string[] project = { "PSAudit", "PSClient", "PSClientInstallers", "PSDatabase", "PSHl7", "PSI", "PSIdentityService", "PSMT", "PSMT-Reports", "PSServer", "PSWS" };
             ProjectSelector.DataSource = project;
-            APFeedbackBox.Hide();
-            APGifZone.Hide(); 
-            
-            
+            APGifZone.Hide();
+
+
         }
 
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
@@ -34,55 +33,77 @@ namespace RegressionSuiteTestHarness
 
         private void button1_Click(object sender, EventArgs e)
         {
-            DialogResult result = openFileDialog1.ShowDialog(); 
-            if(result == DialogResult.OK)
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
             {
                 string file = openFileDialog1.FileName;
                 ProjectSuiteSelector.Clear();
-                ProjectSuiteSelector.Text = file; 
+                ProjectSuiteSelector.Text = file;
+                PutTheGifIn("https://78.media.tumblr.com/tumblr_m7u8dektwm1rbbzboo1_500.gif");
+                APFeedbackBox.Show(); 
+                APFeedbackBox.Text = "Yeah Baby, great project suite selection"; 
             }
-            Console.WriteLine(result); 
+            Console.WriteLine(result);
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-           
+
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            PutTheGifIn("https://media3.giphy.com/media/XenWVVdSzaxLW/giphy.gif");
+            APFeedbackBox.Text = "Great Project Selection"; 
         }
 
-        
+
         private void button2_Click(object sender, EventArgs e)
         {
             using (PowerShell shellPower = PowerShell.Create())
             {
+                string VersionArg = "NA";
+                string BranchArg = "NA";
+                string DevArg = "NA";
                 string tePath = "C:\\Program Files (x86)\\SmartBear\\TestExecute 12\\Bin\\TestExecute.exe";
                 string GUID = Guid.NewGuid().ToString();
                 string localIP = GetIP();
                 string hostName = Dns.GetHostEntry("localhost").HostName;
                 if (!File.Exists(ProjectSuiteSelector.Text))
-                    {
+                {
                     MessageBox.Show("Please Select a Valid Project", "You done messed up now",
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return; 
-                    }
-                string arguments = string.Format("&\"{0} \"{1}\"\" /run /project:{2} /exit BULLSHIT {3} {4} {5}\"\"", tePath, ProjectSuiteSelector.Text, ProjectSelector.Text, GUID, localIP, hostName); 
+                    return;
+                }
+                if (!String.IsNullOrWhiteSpace(Branch.Text))
+
+                {
+                    BranchArg = QuoteWrap(Branch.Text);
+                }
+                if (!String.IsNullOrWhiteSpace(Dev.Text))
+                {
+                    DevArg = QuoteWrap(Dev.Text);
+                }
+                if (!String.IsNullOrWhiteSpace(VersionBox.Text))
+                {
+                    VersionArg = QuoteWrap(VersionBox.Text); 
+                } 
+
+                
+                string arguments = string.Format("&\"{0} \"{1}\"\" /run /project:{2} /exit BULLSHIT {3} {4} {5} {6} {7} {8}\"\"", tePath, ProjectSuiteSelector.Text, ProjectSelector.Text, GUID, localIP, hostName, VersionArg, BranchArg, DevArg);
                 shellPower.AddScript(arguments);
-                APFeedbackBox.Show();
-                APFeedbackBox.Text = string.Format("Oh Behave baby, suite lanuched \n Your GUID is {0}", GUID); 
-                APGifZone.Show(); 
-                shellPower.Invoke(); 
+                APFeedbackBox.Text = string.Format("Oh Behave baby, suite launched \n Your GUID is {0}", GUID);
+                shellPower.Invoke();
+                PutTheGifIn("https://www.reactiongifs.us/wp-content/uploads/2014/09/shocked_austin_powers.gif");
+
 
             }
-        
+
         }
 
         public static string GetIP()
         {
-            var host = Dns.GetHostEntry(Dns.GetHostName()); 
+            var host = Dns.GetHostEntry(Dns.GetHostName());
             foreach (var ip in host.AddressList)
             {
                 if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
@@ -90,7 +111,7 @@ namespace RegressionSuiteTestHarness
                     return ip.ToString();
                 }
             }
-            throw new Exception("Shit broke"); 
+            throw new Exception("Shit broke");
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -100,7 +121,7 @@ namespace RegressionSuiteTestHarness
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         private void textBox2_TextChanged_1(object sender, EventArgs e)
@@ -111,6 +132,48 @@ namespace RegressionSuiteTestHarness
         private void pictureBox1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            APGifZone.Hide();
+            APFeedbackBox.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public static string QuoteWrap(string input)
+        {
+            input = "\"" + input + "\""; 
+            return input;
+        }
+
+        public void PutTheGifIn(string URL)
+        {
+            APGifZone.ImageLocation = URL;
+            APGifZone.SizeMode = PictureBoxSizeMode.StretchImage;
+            APGifZone.Show();
+        }
+
+        private void Branch_TextChanged(object sender, EventArgs e)
+        {
+            PutTheGifIn("https://media.giphy.com/media/xUPGcfe7OAEDKUXQPK/giphy.gif");
+            APFeedbackBox.Text = "Great Branch Baby"; 
+        }
+
+        private void Dev_TextChanged(object sender, EventArgs e)
+        {
+            PutTheGifIn("https://media.giphy.com/media/l0MYC0LajbaPoEADu/giphy.gif");
+            APFeedbackBox.Text = "Great Dev Baby"; 
+        }
+
+        private void VersionBox_TextChanged(object sender, EventArgs e)
+        {
+            PutTheGifIn("https://media.giphy.com/media/l1KVb2dUcmuGG4tby/giphy.gif");
+            APFeedbackBox.Text ="Great Version Baby";
         }
     }
 }
